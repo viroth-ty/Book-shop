4caf6b07da69f380bda74c94611f9781fb59efb8
viroth <viroth.ty@codingate.com>
2022-02-07 22:34:18 +0700

    Set up and import library

---
+++ app/src/main/java/org/viroth/bookstore/app/MainActivity.kt
@@ -0,0 +1,11 @@
+package org.viroth.bookstore.app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
